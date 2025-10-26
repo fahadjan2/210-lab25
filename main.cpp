e767cc3813975002f88b3817b3b0e58215dacc21
fahadjan2 <fahadfawad2006@gmail.com>
2025-10-26 01:28:07 -0700

    Commit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,30 @@
+// COMSC-210 | Lab 25 | Fahad Fawad Ahmad 
+// IDE used: Visual Studio
 #include <iostream>
+#include <fstream>
+#include <set>
+#include <vector>
+#include <list>
 #include <chrono>
 using namespace std;
 
 int main() {
+    ifstream file("codes.txt");
+    vector<string> vect;
+    list<string> list;
+    set<string> set;
     
+    cout << "Test" << endl;
+    string value;
+    while (file >> value) {
+        vect.push_back(value);
+        list.push_back(value);
+        set.insert(value);
 
+        cout << value << endl;
+    };
+
+    file.close();
     return 0;
 }
 
