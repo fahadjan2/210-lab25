267598920f1a049fe43eb98447c8d4082a5c6e1e
fahadjan2 <fahadfawad2006@gmail.com>
2025-10-26 02:25:19 -0700

    Commit

--- main.cpp
+++ main.cpp
@@ -10,13 +10,13 @@
 using namespace std;
 using namespace std::chrono;
 
-void vectorracer(const string filename, vector<string> &vect);
-void listracer(const string filename, list<string> &list);
-void setracer(const string filename, set<string> &set);
-
 struct Durations {
     milliseconds read, sort, insert, delet;
 };
+
+Durations vectorracer(const string filename, vector<string> &vect);
+Durations listracer(const string filename, list<string> &list);
+Durations setracer(const string filename, set<string> &set);
 
 int main() {
     string filename = "codes.txt";
@@ -24,16 +24,19 @@ int main() {
     list<string> list;
     set<string> set;
     
-    cout << "Test111" << endl;
+    cout << "testers" << endl;
     int ssortduration = -1;
-    
-
     Durations vDur = vectorracer(filename, vect);
     Durations lDur = listracer(filename, list);
     Durations sDur = setracer(filename, set);
 
-    cout << "Time taken: " <<  << " milliseconds" << endl;
-    return 0;
+    //Output
+    /*cout << "Operation\tVector\tList\tSet" << endl;
+    cout << "Read\t" << vDur.read.count() << "\t" << lDur.read.count() << "\t" << sDur.read.count() << endl;
+    cout << "Sort\t" << vDur.sort.count() << "\t" << lDur.sort.count() << "\t" << ssortduration << endl;
+    cout << "Insert\t" << vDur.insert.count() << "\t" << lDur.insert.count() << "\t" << sDur.insert.count() << endl;
+    cout << "Delete\t" << vDur.delet.count() << "\t" << lDur.delet.count() << "\t" << sDur.delet.count() << endl;
+    */return 0;
 }
 
 Durations vectorreader(const string &filename, vector<string> &vect) {
@@ -48,23 +51,24 @@ Durations vectorreader(const string &filename, vector<string> &vect) {
     auto readduration = duration_cast<milliseconds>(end - start);
 
     //Sort
-    auto start = high_resolution_clock::now();
+    start = high_resolution_clock::now();
     sort(vect.begin(), vect.end());
+    end = high_resolution_clock::now();
     auto sortduration = duration_cast<milliseconds>(end - start);
-
+        
     //Insert
     value = "TESTCODE";
-    auto start = high_resolution_clock::now();
+    start = high_resolution_clock::now();
     auto it = vect.begin();
     advance(it, vect.size() / 2);
     vect.insert(it, value);
-    auto end = high_resolution_clock::now();
+    end = high_resolution_clock::now();
     auto insertduration = duration_cast<milliseconds>(end - start);
 
     //Delete
-    auto start = high_resolution_clock::now();
+    start = high_resolution_clock::now();
     vect.erase(it);
-    auto end = high_resolution_clock::now();
+    end = high_resolution_clock::now();
     auto deleteduration = duration_cast<milliseconds>(end - start);
 
     //Struct duration set
@@ -89,17 +93,17 @@ Durations setreader(const string &filename, set<string> &set) {
 
     //Insert
     value = "TESTCODE";
-    auto start = high_resolution_clock::now();
+    start = high_resolution_clock::now();
     auto it = set.begin();
     advance(it, set.size() / 2);
     set.insert(it, value);
-    auto end = high_resolution_clock::now();
+    end = high_resolution_clock::now();
     auto insertduration = duration_cast<milliseconds>(end - start);
 
     //Delete
-    auto start = high_resolution_clock::now();
+    start = high_resolution_clock::now();
     set.erase(it);
-    auto end = high_resolution_clock::now();
+    end = high_resolution_clock::now();
     auto deleteduration = duration_cast<milliseconds>(end - start);
 
     //Struct duration set
@@ -122,24 +126,24 @@ Durations listreader(const string &filename, list<string> &list) {
     auto readduration = duration_cast<milliseconds>(end - start);
 
     //Sort
-    auto start = high_resolution_clock::now();
+    start = high_resolution_clock::now();
     list.sort();
-    auto end = high_resolution_clock::now();
+    end = high_resolution_clock::now();
     auto sortduration = duration_cast<milliseconds>(end - start);
 
     //Insert
     value = "TESTCODE";
-    auto start = high_resolution_clock::now();
+    start = high_resolution_clock::now();
     auto it = list.begin();
     advance(it, list.size() / 2);
     list.insert(it, value);
-    auto end = high_resolution_clock::now();
+    end = high_resolution_clock::now();
     auto insertduration = duration_cast<milliseconds>(end - start);
 
     //Delete
-    auto start = high_resolution_clock::now();
+    start = high_resolution_clock::now();
     list.erase(it);
-    auto end = high_resolution_clock::now();
+    end = high_resolution_clock::now();
     auto deleteduration = duration_cast<milliseconds>(end - start);
 
     //Struct duration set
