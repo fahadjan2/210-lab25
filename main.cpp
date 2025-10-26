470a81e44efa2c513232f6f3ed7c67a975d92230
fahadjan2 <fahadfawad2006@gmail.com>
2025-10-26 01:45:35 -0700

    Commit

--- main.cpp
+++ main.cpp
@@ -9,53 +9,70 @@
 using namespace std;
 using namespace std::chrono;
 
-void vectorreader(const string filename, vector<string> &vect);
-void listreader(const string filename, list<string> &list);
-void setreader(const string filename, set<string> &set);
+void vectorracer(const string filename, vector<string> &vect);
+void listracer(const string filename, list<string> &list);
+void setracer(const string filename, set<string> &set);
 
 int main() {
-    ifstream file("codes.txt");
+    string filename = "codes.txt";
     vector<string> vect;
     list<string> list;
     set<string> set;
-
-    auto start = high_resolution_clock::now();
     
-    cout << "Test" << endl;
+    cout << "Test111" << endl;
+    int vduration, lduration, sduration;
+    vduration = vectorracer(filename, vect);
+    lduration = listracer(filename, list);
+    sduration = setracer(filename, set);
+
+    cout << "Time taken: " <<  << " milliseconds" << endl;
+    return 0;
+}
+
+void vectorreader(const string &filename, vector<string> &vect) {
+    ifstream file(filename);
+    
+    auto start = high_resolution_clock::now();
+
     string value;
     while (file >> value) {
         vect.push_back(value);
-        list.push_back(value);
+    };
+
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(end - start);
+
+    return duration.count();
+}
+
+void setreader(const string &filename, set<string> &set) {
+    ifstream file(filename);
+    
+    auto start = high_resolution_clock::now();
+    
+    string value;
+    while (file >> value) {
         set.insert(value);
     };
 
     auto end = high_resolution_clock::now();
-
- 
     auto duration = duration_cast<milliseconds>(end - start);
 
-
-    std::cout << "Time taken: " << duration.count() << " milliseconds\n";
-
-    file.close();
-    return 0;
+    return duration.count();
 }
 
-/* syntax examples:
-auto start = high_resolution_clock::now()
-auto end = high_resolution_clock::now()
-auto duration = duration_cast<milliseconds>(end - start)
-duration.count() references elapsed milliseconds
-*/
+void listreader(const string &filename, list<string> &list) {
+    ifstream file(filename);
+    
+    auto start = high_resolution_clock::now();
+    
+    string value;
+    while (file >> value) {
+        list.push_back(value);
+    };
 
-void vectorreader(const string filename, vector<string> &vect) {
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(end - start);
 
-}
-
-void setreader(const string filename, set<string> &set) {
-
-}
-
-void listreader(const string filename, list<string> &list) {
-
+    return duration.count();
 }
